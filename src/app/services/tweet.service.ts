26afe24ad55bd6db580ce709281f1d1c455fb21d
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-09-11 12:34:21 -0400

    Add Tweet service

---
+++ src/app/services/tweet.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map, catchError} from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+
+export class TweetService {
+  mainURL: String = environment.BASE_URL;
+  constructor(private http: Http) {}
+
+  handleError(e) {
+    return throwError(e.json().message);
+  }
+
+  postTweet(tweet) {
+    return this.http.post(`${this.mainURL}/tweet`, tweet, { withCredentials: true })
+    .pipe(map(res => res.json()), catchError(this.handleError));
+  }
+}
