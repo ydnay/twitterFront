25f9d1424da4b7f060c77a206d2fb1681120e18a
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-09-13 14:27:23 -0400

    template-parse-error-fixed

--- src/app/mini-profile/mini-profile.component.ts
+++ src/app/mini-profile/mini-profile.component.ts
@@ -3,16 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mini-profile',
   templateUrl: './mini-profile.component.html',
-  template: `
-  <h1> {{user.username}} </h1>
-    <div>
-      
-        <h4> {{ user.tweets }} </h4>
-        <h4> {{ user.following }} </h4>
-        <h4> {{ user.followers }} </h4>
-      </div>
-    </div>
-  `,
+
   styleUrls: ['./mini-profile.component.css']
 })
 export class MiniProfileComponent implements OnInit {
